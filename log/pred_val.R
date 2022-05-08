39fc087042266e0e5e586f827f99b97009d8a565
Subrata <subrata@iastate.edu>
2022-05-08 18:36:56 -0500

    Intermed

--- log/pred_val.R
+++ log/pred_val.R
@@ -1,6 +1,14 @@
 
-new_styles <- -1*c(9818:9824,9829,9830,9831)
-plot(sort(rnorm(4)), pch=new_styles)
+new_styles <- -1*c(9818:9824,9829,9830,9831,9832,9833)
+new_styles <- -1*c(9828,9829,9830,9831,9832)
+new_styles <- -1*c(9824,9827,9829,9830,9831)
+plot(sort(rnorm(25)), pch=new_styles)
+plot(sort(rnorm(25)), pch=c("☺", "❤", "✌", "❄", "✈"))
+
+plot(sort(rnorm(25)), pch=c("❤", "♠", "♦", "♣"))
+
+
+
 
 
 noise_vec <- c(1, 2.5, 5, 7.5, 10)
@@ -66,13 +74,16 @@ tmp <- all_dat %>%
   pivot_longer(!c(DL, measures, errs, method), names_to = "img", values_to = "vals") %>% 
   filter(measures == "MAPE") %>% 
   pivot_wider(names_from = img, values_from = vals) %>% 
+  select(-c(measures)) %>% 
   arrange(method) %>%
   arrange(errs) %>%
-  select(-c(measures)) %>% 
-  relocate(DL, .after=method)
+  relocate(DL, .after=method) %>% 
+  mutate(DL = ifelse(DL, "DL", "")) %>% 
+  mutate(method = interaction(DL, method)) %>% 
+  select(-DL)
 
 library(xtable)
-print(xtable(tmp, digits=c(1,1,0,0,2,2,2,2,2,2,2,2,2)), include.rownames=FALSE)
+print(xtable(tmp, digits=c(1,1,0,2,2,2,2,2,2,2,2,2)), include.rownames=FALSE)
 
 
 
@@ -195,7 +206,7 @@ print(xtable(tmp, digits=c(1,1,0,0,2,2,2,2,2,2,2,2,2)), include.rownames=FALSE)
 
 
 names(all_dat)[5:13] <- paste0('Test image ', 1:9)
-all_dat$DL <- ifelse(all_dat$DL, "DL", "No DL")
+# all_dat$DL <- ifelse(all_dat$DL, "DL", "No DL")
 
 
 
@@ -203,7 +214,10 @@ all_dat$DL <- ifelse(all_dat$DL, "DL", "No DL")
 
 tmp_dat <- all_dat %>%
   pivot_longer(!c(DL, measures, errs, method), names_to = "img", values_to = "vals") %>% 
-  filter(img == "Test image 1" | img == "Test image 2" | img == "Test image 6" | img == "Test image 8") 
+  filter(img == "Test image 1" | img == "Test image 2" | img == "Test image 6" | img == "Test image 8") %>% 
+  mutate(method_old = method) %>%
+  mutate(method = ifelse(DL, paste0("DL-", method), method )) 
+
 
 library(RColorBrewer)
 my_cols <-  brewer.pal(5, 'Dark2')
@@ -212,8 +226,8 @@ my_cols <-  brewer.pal(5, 'Dark2')
 
 p <- tmp_dat %>%
   filter(measures == "MAPE") %>%
-  ggplot(aes(x = interaction(method, DL), y = vals, group = errs, shape=interaction(method, DL))) +
-    geom_point(aes(color=factor(errs), size=3)) + 
+  ggplot(aes(x = method, y = vals, group = errs, shape=method)) +
+    geom_point(aes(color=factor(errs), size=1)) + 
     scale_shape_manual(values = new_styles[1:4]) + 
     scale_colour_manual(values=my_cols) + 
     facet_grid(cols = vars(img)) +
@@ -224,14 +238,14 @@ p <- tmp_dat %>%
 p1 <- p + labs( colour = "Noise percentage", shape = "Method") + 
   guides(linetype = FALSE, size="none") + 
   guides(shape = guide_legend(override.aes = list(size = 5))) + xlab('Method') + ylab("MAPE")
-
+p1
 ggsave('MAPE.jpg')
 
 
 p <- tmp_dat %>%
   filter(measures == "RMSPE") %>%
-  ggplot(aes(x = interaction(method, DL), y = vals, group = errs, shape=interaction(method, DL))) +
-    geom_point(aes(color=factor(errs), size=3)) + 
+  ggplot(aes(x = method, y = vals, group = errs, shape=method)) +
+    geom_point(aes(color=factor(errs), size=1)) + 
     scale_shape_manual(values = new_styles[1:4]) + 
     scale_colour_manual(values=my_cols) + 
     facet_grid(cols = vars(img)) +
@@ -239,18 +253,18 @@ p <- tmp_dat %>%
     theme_minimal() + theme(panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"), 
                                 plot.background = element_rect(fill = "white"))
 
-p1 <- p + labs( colour = "Noise percentage", shape = "Method") + 
+p2 <- p + labs( colour = "Noise percentage", shape = "Method") + 
   guides(linetype = FALSE, size="none") + 
   guides(shape = guide_legend(override.aes = list(size = 5))) + xlab('Method') + ylab("RMSPE")
-
+p2
 ggsave('RMSPE.jpg')
 
 
 
 p <- tmp_dat %>%
   filter(measures == "SSIM") %>%
-  ggplot(aes(x = interaction(method, DL), y = vals, group = errs, shape=interaction(method, DL))) +
-    geom_point(aes(color=factor(errs), size=3)) + 
+  ggplot(aes(x = method, y = vals, group = errs, shape=method)) +
+    geom_point(aes(color=factor(errs), size=1)) + 
     scale_shape_manual(values = new_styles[1:4]) + 
     scale_colour_manual(values=my_cols) + 
     facet_grid(cols = vars(img)) +
@@ -258,15 +272,69 @@ p <- tmp_dat %>%
     theme_minimal() + theme(panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"), 
                                 plot.background = element_rect(fill = "white"))
 
-p + labs( colour = "Noise percentage", shape = "Method") + 
+p3 <- p + labs(colour = "Noise percentage", shape = "Method") + 
   guides(linetype = FALSE, size="none") + 
   guides(shape = guide_legend(override.aes = list(size = 5))) + xlab('Method') + ylab("SSIM")
-
+p3
 ggsave('SSIM.jpg')
 
 
+library(patchwork)
+p1_new <- p1 + theme(legend.position="none", 
+              axis.title.x=element_blank(), 
+              axis.text.x=element_blank()) 
+p2_new <- p2 + theme(legend.position="none", 
+              strip.background = element_blank(),
+              strip.text.x = element_blank(),
+              axis.title.x=element_blank(), 
+              axis.text.x=element_blank())
+p3_new <- p3 + theme(legend.position="none", 
+              strip.background = element_blank(),
+              strip.text.x = element_blank(), 
+              axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))
+
+p1_new / p2_new / p3_new
+
+ggsave('all-SE.png', scale=0.75)
 
 
 
 
 
+
+
+
+p <- tmp_dat %>%
+  ggplot(aes(x = interaction(method, DL), y = vals, group = errs, shape=interaction(method, DL))) +
+    geom_point(aes(color=factor(errs), size=3)) + 
+    scale_shape_manual(values = new_styles[1:4]) + 
+    scale_colour_manual(values=my_cols) + 
+    facet_grid(rows = vars(measures), cols = vars(img), scale='free_y') + 
+    geom_line(aes(linetype = factor(errs), color = factor(errs))) +
+    theme_minimal() + theme(panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"), 
+                                plot.background = element_rect(fill = "white"),
+                                strip.text.y = element_text(angle = 0)) + 
+  labs(colour = "Noise percentage", shape = "Method") + 
+  guides(linetype = FALSE, size="none") + 
+  guides(shape = guide_legend(override.aes = list(size = 5))) + 
+  xlab('Method') + ylab("SSIM")
+
+
+
+
+
+
+
+tmp_dat %>%
+  ggplot(aes(x = interaction(method, DL), y = vals, group = errs, shape=interaction(method, DL))) +
+    geom_point(aes(color=factor(errs), size=3)) + 
+    scale_shape_manual(values = new_styles[1:4]) + 
+    scale_colour_manual(values=my_cols) + 
+    facet_wrap(~measures * img, strip.position = c("left")) + 
+    geom_line(aes(linetype = factor(errs), color = factor(errs))) +
+    theme_minimal() + theme(panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"), 
+                                plot.background = element_rect(fill = "white"),
+                                strip.text.y = element_text(angle = 0)) + 
+                                ylab(NULL) +
+     theme(strip.background = element_blank(),
+           strip.placement = "outside")
