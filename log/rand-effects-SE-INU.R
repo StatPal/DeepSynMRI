e27e4f35950ef99e8bbe97054bf06a017ed425c5
Subrata <subrata@iastate.edu>
2022-07-24 06:07:02 -0500

    Real and FLASH INU etc added.

---
+++ log/rand-effects-SE-INU.R
@@ -0,0 +1,246 @@
+## FLASH
+
+INU_vec <- c("00", "10", "20")
+all_normal <- c()
+all_DL     <- c()
+
+for(i in 1:3){
+    f1 <- paste0("values/pred-noise-5-INU-", INU_vec[i], ".csv")
+    f2 <- paste0("values/DL-pred-noise-5-INU-", INU_vec[i], ".csv")
+
+    all_normal <- rbind(all_normal, as.matrix(read.csv(f1, header = F))[c(6,7,3, 13,14,10), ])
+    all_DL     <- rbind(all_DL    , as.matrix(read.csv(f2, header = F))[c(6,7,3, 13,14,10), ])
+}
+
+all_normal <- data.frame(all_normal)
+all_DL <- data.frame(all_DL)
+
+method <- rep(c("LS", "MLE"), each=3, times=3)  # The second will be 3
+measures <- rep(c("MAPE", "RMSPE", "SSIM"), 3 * 2)
+INU <- rep(INU_vec, each = 3 * 2)
+DL <- rep(c(F, T), each = 3 * 3 * 2)
+
+all_normal <- cbind(measures, INU, method, all_normal)
+all_DL     <- cbind(measures, INU, method, all_DL    )
+
+all_dat <- cbind(DL, rbind(all_normal, all_DL))
+all_dat$INU <- as.numeric(all_dat$INU)
+
+rm(DL)
+rm(measures)
+rm(INU)
+
+
+
+
+
+
+
+
+
+library(tidyverse)
+library(lme4)
+
+
+tmp_dat <- all_dat %>%
+    filter(measures == "SSIM") %>%
+    filter(method == "LS") %>%
+    pivot_longer(!c(DL, measures, INU, method), names_to = "img", values_to = "vals") 
+
+
+tmp_dat$INUf <- factor(tmp_dat$INU)
+(mod_test <- lmer(vals ~ DL * INUf + (1 | img), tmp_dat))   # random effect for intercept
+
+library(emmeans)
+emmeans_1 <- emmeans(mod_test, ~ DL | INUf)
+pairs(emmeans_1)
+xtable::xtable(pairs(emmeans_1))
+
+library(ggResidpanel)
+resid_panel(mod_test)
+
+
+library(qqplotr)
+type = "response"
+model = mod_test
+model_values <- data.frame(Residual = ggResidpanel:::helper_resid(type = "response", model = mod_test))
+r_label <- ggResidpanel:::helper_label(type, model)
+data_add <- ggResidpanel:::helper_plotly_label(model)
+model_values <- cbind(model_values, data_add)
+names(model_values)[which(names(model_values) == "data_add")] <- "Data"
+model_values <- model_values[order(model_values$Residual), ]
+plot <- ggplot(data = model_values, mapping = aes_string(sample = "Residual", 
+    label = "Data")) + stat_qq_point() + labs(x = "Theoretical Quantiles", 
+    y = "Sample Quantiles")
+plot_data <- ggplot_build(plot)
+model_values$Theoretical <- plot_data[[1]][[1]]$theoretical
+model_values$Residual_Plot <- model_values$Residual
+
+
+p_SSIM <- ggplot(data = model_values, mapping = aes_string(sample = "Residual_Plot", 
+    label = "Data")) + stat_qq_point() + 
+    labs(x = "Theoretical Quantiles", y = "Sample Quantiles") + 
+    stat_qq_line(color = "blue", size = 0.5) + 
+    theme_minimal() + theme(panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"), 
+                                plot.background = element_rect(fill = "white"),
+                                axis.text = element_text(size = 15),
+                                axis.title = element_text(size = 20))
+
+p_SSIM_band <- ggplot(data = model_values, mapping = aes_string(sample = "Residual_Plot", 
+    label = "Data")) + stat_qq_band() + stat_qq_point() + 
+    labs(x = "Theoretical Quantiles", y = "Sample Quantiles") + 
+    stat_qq_line(color = "blue", size = 0.5) + 
+    theme_minimal() + theme(panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"), 
+                                plot.background = element_rect(fill = "white"),
+                                axis.text = element_text(size = 15),
+                                axis.title = element_text(size = 20))
+
+p_SSIM
+p_SSIM_band
+
+
+
+
+
+
+
+
+
+tmp_dat <- all_dat %>%
+    filter(measures == "MAPE") %>%
+    filter(method == "LS") %>%
+    pivot_longer(!c(DL, measures, INU, method), names_to = "img", values_to = "vals") 
+
+
+tmp_dat$INUf <- factor(tmp_dat$INU)
+(mod_test <- lmer(vals ~ DL * INUf + (1 | img), tmp_dat))   # random effect for intercept
+
+library(emmeans)
+emmeans_1 <- emmeans(mod_test, ~ DL | INUf)
+pairs(emmeans_1)
+xtable::xtable(pairs(emmeans_1))
+
+library(ggResidpanel)
+resid_panel(mod_test)
+
+
+library(qqplotr)
+type = "response"
+model = mod_test
+model_values <- data.frame(Residual = ggResidpanel:::helper_resid(type = "response", model = mod_test))
+r_label <- ggResidpanel:::helper_label(type, model)
+data_add <- ggResidpanel:::helper_plotly_label(model)
+model_values <- cbind(model_values, data_add)
+names(model_values)[which(names(model_values) == "data_add")] <- "Data"
+model_values <- model_values[order(model_values$Residual), ]
+plot <- ggplot(data = model_values, mapping = aes_string(sample = "Residual", 
+    label = "Data")) + stat_qq_point() + labs(x = "Theoretical Quantiles", 
+    y = "Sample Quantiles")
+plot_data <- ggplot_build(plot)
+model_values$Theoretical <- plot_data[[1]][[1]]$theoretical
+model_values$Residual_Plot <- model_values$Residual
+
+
+p_MAPE <- ggplot(data = model_values, mapping = aes_string(sample = "Residual_Plot", 
+    label = "Data")) + stat_qq_point() + 
+    labs(x = "Theoretical Quantiles", y = "Sample Quantiles") + 
+    stat_qq_line(color = "blue", size = 0.5) + 
+    theme_minimal() + theme(panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"), 
+                                plot.background = element_rect(fill = "white"),
+                                axis.text = element_text(size = 15),
+                                axis.title = element_text(size = 20))
+
+p_MAPE_band <- ggplot(data = model_values, mapping = aes_string(sample = "Residual_Plot", 
+    label = "Data")) + stat_qq_band() + stat_qq_point() + 
+    labs(x = "Theoretical Quantiles", y = "Sample Quantiles") + 
+    stat_qq_line(color = "blue", size = 0.5) + 
+    theme_minimal() + theme(panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"), 
+                                plot.background = element_rect(fill = "white"),
+                                axis.text = element_text(size = 15),
+                                axis.title = element_text(size = 20))
+
+p_MAPE
+p_MAPE_band
+
+
+
+
+
+tmp_dat <- all_dat %>%
+    filter(measures == "RMSPE") %>%
+    filter(method == "LS") %>%
+    pivot_longer(!c(DL, measures, INU, method), names_to = "img", values_to = "vals") 
+
+
+tmp_dat$INUf <- factor(tmp_dat$INU)
+(mod_test <- lmer(vals ~ DL * INUf + (1 | img), tmp_dat))   # random effect for intercept
+
+library(emmeans)
+emmeans_1 <- emmeans(mod_test, ~ DL | INUf)
+pairs(emmeans_1)
+xtable::xtable(pairs(emmeans_1))
+
+library(ggResidpanel)
+resid_panel(mod_test)
+
+
+library(qqplotr)
+type = "response"
+model = mod_test
+model_values <- data.frame(Residual = ggResidpanel:::helper_resid(type = "response", model = mod_test))
+r_label <- ggResidpanel:::helper_label(type, model)
+data_add <- ggResidpanel:::helper_plotly_label(model)
+model_values <- cbind(model_values, data_add)
+names(model_values)[which(names(model_values) == "data_add")] <- "Data"
+model_values <- model_values[order(model_values$Residual), ]
+plot <- ggplot(data = model_values, mapping = aes_string(sample = "Residual", 
+    label = "Data")) + stat_qq_point() + labs(x = "Theoretical Quantiles", 
+    y = "Sample Quantiles")
+plot_data <- ggplot_build(plot)
+model_values$Theoretical <- plot_data[[1]][[1]]$theoretical
+model_values$Residual_Plot <- model_values$Residual
+
+
+p_RMSPE <- ggplot(data = model_values, mapping = aes_string(sample = "Residual_Plot", 
+    label = "Data")) + stat_qq_point() + 
+    labs(x = "Theoretical Quantiles", y = "Sample Quantiles") + 
+    stat_qq_line(color = "blue", size = 0.5) + 
+    theme_minimal() + theme(panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"), 
+                                plot.background = element_rect(fill = "white"),
+                                axis.text = element_text(size = 15),
+                                axis.title = element_text(size = 20))
+
+p_RMSPE_band <- ggplot(data = model_values, mapping = aes_string(sample = "Residual_Plot", 
+    label = "Data")) + stat_qq_band() + stat_qq_point() + 
+    labs(x = "Theoretical Quantiles", y = "Sample Quantiles") + 
+    stat_qq_line(color = "blue", size = 0.5) + 
+    theme_minimal() + theme(panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"), 
+                                plot.background = element_rect(fill = "white"),
+                                axis.text = element_text(size = 15),
+                                axis.title = element_text(size = 20))
+
+p_RMSPE
+p_RMSPE_band
+
+
+
+
+
+pdf('qqplots-SE-INU.pdf')
+p_MAPE_band
+p_RMSPE_band
+p_SSIM_band
+
+p_MAPE
+p_RMSPE
+p_SSIM
+
+dev.off()
+
+
+system("pdfcrop qqplots-SE-INU.pdf")
+
+
+
+
+
---
+++ log/real.R
@@ -0,0 +1,202 @@
+pred <- as.matrix(read.csv("values/real-performances.csv", header=F))
+
+
+perf_LS <- pred[c(6,7,3), ]
+perf_MLE <- pred[c(13,14,10), ]
+perf_DL_LS <- pred[c(20,21,17), ]
+perf_DL_MLE <- pred[c(27,28,24), ]
+
+perf_new <- rbind(perf_LS, perf_MLE, perf_DL_LS, perf_DL_MLE)
+
+method <- rep(c("LS", "MLE"), each=3, times=2)  # The second will be 3
+measures <- rep(c("MAPE", "RMSPE", "SSIM"), 2 * 2)
+DL <- rep(c(F, T), each = 3 * 2)
+
+
+all_dat <- data.frame(measures, method, DL, perf_new)
+
+rm(DL)
+rm(measures)
+
+
+## Plots
+library(tidyverse)
+library(tidyr)
+library(ggplot2)
+library(xtable)
+
+colnames(all_dat)[4:12] <- paste0('Test image ', 1:9)
+all_dat_check <- tibble(all_dat)
+all_dat_old <- all_dat_check
+
+
+
+
+
+
+tmp <- all_dat_check %>%
+  pivot_longer(!c(DL, measures, method), names_to = "img", values_to = "vals") %>% 
+  filter(measures == "MAPE") %>% 
+  pivot_wider(names_from = img, values_from = vals) %>% 
+  select(-c(measures)) %>% 
+  arrange(method) %>%
+  relocate(DL, .after=method) %>% 
+  mutate(DL = ifelse(DL, "DL", "")) %>% 
+  mutate(method = interaction(DL, method)) %>% 
+  mutate_if(is.numeric, ~ . * 100) %>%
+  select(-DL)
+
+print(xtable(tmp, digits=c(1,0,2,2,2,2,2,2,2,2,2)), include.rownames=FALSE)
+
+
+
+
+
+
+
+
+library(stringr)
+
+TE_TR <- c("15/0.6", "20/0.6", "10/1", "30/1", "40/1", "10/2", "40/2", "60/3", "100/3")
+TE_TR_names  <- paste0('TE/TR = ', TE_TR[1:9])
+
+tmp_dat_old <- all_dat %>%
+  pivot_longer(!c(DL, measures, method), names_to = "img", values_to = "vals") %>% 
+  # filter(img != "Test image 6") %>%   # Remove 6-th test image as it ewas flagged
+  mutate(method_old = method) %>%
+  mutate(method = ifelse(DL, paste0("DL+", method), method )) 
+
+tmp_dat <- tmp_dat_old
+tmp_dat$img_vals  <-  TE_TR_names[as.numeric( str_split_fixed(tmp_dat_old$img, ' ', n=3)[,3] )]
+
+
+
+library(RColorBrewer)
+my_cols <-  brewer.pal(5, 'Dark2')
+new_styles <- 15:18
+
+
+
+tmp_dat %>%
+  filter(measures == "MAPE") %>%
+  ggplot() + 
+    aes(x = img, y = vals, shape=method, group = method, linetype = method) +
+    geom_point(size=3) + 
+    scale_shape_manual(values = new_styles[1:4]) + 
+    scale_colour_manual(values=my_cols) + 
+    geom_line()
+
+
+
+
+p <- tmp_dat %>%
+  filter(measures == "MAPE") %>%
+  ggplot() + 
+    aes(x = img_vals, y = vals, shape=method, group = method, linetype = method, color = method) +
+    geom_point(size=3) + 
+    scale_shape_manual(values = new_styles[1:4]) + 
+    scale_colour_manual(values=my_cols) + 
+    geom_line() +
+    theme_minimal() + theme(panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"), 
+                                plot.background = element_rect(fill = "white"),
+                                axis.text.x = element_text(angle = 60, vjust = 1, hjust=1))
+p1 <- p + 
+  guides(size = FALSE) + 
+  labs(color  = "Method", linetype = "Method", shape = "Method") + 
+  xlab('Image') + ylab("MAPE")
+p1
+ggsave('real-MAPE.jpg')
+
+
+p <- tmp_dat %>%
+  filter(measures == "RMSPE") %>%
+  ggplot() + 
+    aes(x = img_vals, y = vals, shape=method, group = method, linetype = method, color = method) +
+    geom_point(size=3) + 
+    scale_shape_manual(values = new_styles[1:4]) + 
+    scale_colour_manual(values=my_cols) + 
+    geom_line() +
+    theme_minimal() + theme(panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"), 
+                                plot.background = element_rect(fill = "white"),
+                                axis.text.x = element_text(angle = 60, vjust = 1, hjust=1))
+p2 <- p + 
+  guides(size = FALSE) + 
+  labs(color  = "Method", linetype = "Method", shape = "Method") + 
+  xlab('\nImage') + ylab("RMSPE")
+p2
+ggsave('real-RMSPE.jpg')
+
+
+
+p <- tmp_dat %>%
+  filter(measures == "SSIM") %>%
+  ggplot() + 
+    aes(x = img_vals, y = vals, shape=method, group = method, linetype = method, color = method) +
+    geom_point(size=3) + 
+    scale_shape_manual(values = new_styles[1:4]) + 
+    scale_colour_manual(values=my_cols) + 
+    geom_line() +
+    theme_minimal() + theme(panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"), 
+                                plot.background = element_rect(fill = "white"),
+                                axis.text.x = element_text(angle = 60, vjust = 1, hjust=1))
+p3 <- p + 
+  guides(size = FALSE) + 
+  labs(color  = "Method", linetype = "Method", shape = "Method") + 
+  xlab('\nImage') + ylab("SSIM")
+p3
+ggsave('real-SSIM.jpg')
+
+
+library(patchwork)
+p1_new <- p1 + theme(legend.position="none", 
+              axis.title.x=element_blank(), 
+              axis.text.x=element_blank()) 
+p2_new <- p2 + theme(legend.position="none", 
+              strip.background = element_blank(),
+              strip.text.x = element_blank(),
+              axis.title.x=element_blank(), 
+              axis.text.x=element_blank())
+p3_new <- p3 + theme(legend.position="none", 
+              strip.background = element_blank(),
+              strip.text.x = element_blank(), 
+              axis.text.x = element_text(angle = 30, vjust = 1, hjust=1))
+
+p1_new / p2_new / p3_new
+
+ggsave('all-real.png', scale=1.0)
+
+
+
+
+tmp <- cowplot::get_legend(p3 + 
+    theme(legend.key.width = unit(1.5,"cm")) + 
+    guides(
+        size = guide_legend(override.aes = list(size = 2)), 
+        linetype = guide_legend(override.aes = list(size = 1.1))))
+ggpubr::as_ggplot(tmp)
+
+library(grid)
+grid.ls(grid.force())
+grid.gedit("key-[-0-9]-1-1", size = unit(5, "mm"))
+g <- grid.grab()
+print(g)
+legend1 <- ggpubr::as_ggplot(g)
+legend1
+ggsave('real-Legend.png', scale=1.0)
+
+
+
+# all_plot <- (p1_new / p2_new / p3_new) + coord_fixed()
+
+layout <- c(
+  area(t = 0, l = 0, b = 2, r = 4),
+  area(t = 3, l = 0, b = 4, r = 4),
+  area(t = 5, l = 0, b = 6, r = 4),
+  area(t = 2, l = 5, b = 6, r = 5)
+)
+p1_new + p2_new + p3_new + legend1 + 
+  plot_layout(design = layout)
+
+ggsave('all-real-Legend.pdf', scale=1.0)
+
+system("pdfcrop all-real-Legend.pdf")
